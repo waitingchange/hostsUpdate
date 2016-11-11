dd8a668fb8f7bd435f909cadbd0bb415ca774822
MacBook Pro <change@MacBook-Pro.local>
2016-11-11 23:03:14 +0800

    simple code for *unix platform

---
+++ hosts.py
@@ -0,0 +1,29 @@
+import urllib
+import os
+import sys
+
+url = 'https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts'
+
+
+def main():
+	page = urllib.urlopen(url)
+	html = page.read()
+	print html
+
+	keyWord = raw_input( 'please input y/n: ' )
+	print 'keyWord is ' , keyWord
+	if keyWord != 'y':
+		print 'unable to continue!'
+		exit()
+
+	print 'continue ...'
+	file_object = open('/tmp/hosts', 'w')
+	file_object.write(html)
+	file_object.close( )
+
+	os.system('sudo mv /tmp/hosts /etc/hosts')
+	os.system('sudo killall -HUP mDNSResponder')
+	print 'complete '
+
+if __name__ == '__main__':
+	main()
